74452f0404d0764807dc3d1f7cc0632240690510
hi33741 <hi33741@naver.com>
2021-08-28 02:18:05 +0900

    login1set

--- src/Page/Auth.js
+++ src/Page/Auth.js
@@ -1,21 +1,52 @@
 //로그인 폼만들꺼
-import React from "react";
+import { useState } from "react";
 
 const Auth = () => {
+  const [eMail, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    console.log(e.target.name);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    console.log(e.target.name);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <from>
-        <input type="email" placeholder="E-Mail" required />
-        <input type="password" required />
+    <form>
+      <div onSubmit={onSubmit}>
+        <input
+          name="email"
+          value={eMail}
+          onChange={handleEmailChange}
+          type="email"
+          placeholder="E-Mail"
+          required
+        />
+
+        <input
+          name="password"
+          value={password}
+          placeholder="password"
+          onChange={handlePasswordChange}
+          type="password"
+          required
+        />
         <input type="submit" value="Log In" />
-      </from>
+      </div>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Github</button>
       </div>
-    </div>
+    </form>
   );
 };
 
 export default Auth;
 //required 반드시 채워서 보내야되는 설정
+//로그인구현할때는 form 태그 추천
