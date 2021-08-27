334c1dd85246e5ab782f96cd30cb345f151dc876
hi33741 <hi33741@naver.com>
2021-08-28 01:46:44 +0900

    loginStart

--- src/Page/Auth.js
+++ src/Page/Auth.js
@@ -1,7 +1,21 @@
+//로그인 폼만들꺼
 import React from "react";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  return (
+    <div>
+      <from>
+        <input type="email" placeholder="E-Mail" required />
+        <input type="password" required />
+        <input type="submit" value="Log In" />
+      </from>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
+//required 반드시 채워서 보내야되는 설정
