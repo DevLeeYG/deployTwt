592f2a38fb9c56a451e536e544f0512ccd2c67c0
hi33741 <hi33741@naver.com>
2021-08-29 16:21:06 +0900

    tweetGet

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -1,20 +1,35 @@
 //홈.. 페이지로 프로필이나 트윗등록이나 여러가지 할수잇는 환경이다
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { dbService } from "../firebase";
 
 const Home = () => {
-  const [tweet, setTweet] = useState("");
+  const [tweet, setTweet] = useState(""); //트윗
+  const [tweets, setTweets] = useState([]); //목록
+
+  const getTweets = async () => {
+    const dbTweet = await dbService.collection("tweets").get(); //데이터베이스 트윗 겟요청
+    dbTweet.forEach((doc) => setTweets((prev) => [doc.data(), prev]));
+  }; //어싱크 어웨이트는 따로뺴서 함수로 만들어주고 유즈이펙트에서써야함
+
+  useEffect(() => {
+    getTweets();
+  }, []);
+  console.log(tweets);
 
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService
-      .collection("twees")
-      .add({ text: tweet, createdAt: Date.now() });
-    setTweet("");
+      .collection("tweets") //컬렉션을 생성한다
+      .add({ text: tweet, createdAt: Date.now() }); //파이어베이스 데이터베이스에 저장//문서생성
+    setTweet(""); //트위한후 초기화
   };
 
   const onChange = (event) => {
-    setTweet(event.target.value);
+    event.preventDefault();
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
   };
 
   return (
